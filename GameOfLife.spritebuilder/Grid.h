46f5748c4ec3bfdcf4492435bd605a9216cba0d4
varun <varunv123@gmail.com>
2015-02-03 18:45:35 -0500

    Varun GameOfLife

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,19 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Varun Vijayraghavan on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+- (void) evolveStep;
+- (void) countNeighbors;
+- (void) updateCreatures;
+
+
+@end
